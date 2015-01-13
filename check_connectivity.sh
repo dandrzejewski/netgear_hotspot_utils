c32da50ad9f45d4be60be210fc7fa9a890c39d77
David Andrzejewski <david@davidandrzejewski.com>
2015-01-13 11:13:41 -0500

    Finished shell script.

--- check_connectivity.sh
+++ check_connectivity.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+PING_HOST="google.com"
+PING_COUNT=10
+REBOOT_CMD="/root/restart_hotspot.py"
+
+
+/bin/ping -c 10 ${PING_HOST}
+
+if [ $? -ne 0 ]; then
+    echo "Unable to ping ${PING_HOST}, rebooting hotspot."
+    ${REBOOT_CMD}
+else
+    echo "Host ${PING_HOST} is reachable, no reboot needed."
+fi
