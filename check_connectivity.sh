1c7faac52b8aeadc245c060b9486553f8b7ae951
David Andrzejewski <david@davidandrzejewski.com>
2015-01-13 11:22:32 -0500

    Updated shell script to dynamically get the right directory name for the python script.

--- check_connectivity.sh
+++ check_connectivity.sh
@@ -2,8 +2,13 @@
 
 PING_HOST="google.com"
 PING_COUNT=10
-REBOOT_CMD="/root/restart_hotspot.py"
+REBOOT_SCRIPT_NAME="hsreboot.py"
 
+
+SCRIPT="$(readlink -f ${BASH_SOURCE[0]})"
+SCRIPT_DIR="$(dirname ${SCRIPT})"
+
+REBOOT_CMD="${SCRIPT_DIR}/${REBOOT_SCRIPT_NAME}"
 
 /bin/ping -c 10 ${PING_HOST}
 
